34077784468964d65e942dbe91396d265686f155
root <root@ip-10-0-0-16.eu-west-2.compute.internal>
2023-05-13 13:51:10 +0000

    paths for pretrained concat resnet all labels added to bash scripts

--- CPS/TorchSemiSeg/exp_city/city8_res50v3_CPS_CutMix/concat_all/script_depth_concat_fullresnet.sh
+++ CPS/TorchSemiSeg/exp_city/city8_res50v3_CPS_CutMix/concat_all/script_depth_concat_fullresnet.sh
@@ -5,20 +5,20 @@ export NGPUS=1
 export learning_rate=0.02
 export batch_size=1
 export snapshot_iter=2
-export epochs=137
+export epochs=100
 export ratio=16
 export CPU_DIST_ONLY='False'
 export WORLD_SIZE=1
 export mode="All-Labels-Concat-Semi-Supervision-FullResnet"
 export debug="False"
 export full_depth_resnet="True"
-export load_checkpoint="False"
-export load_depth_checkpoint="False"
+export load_checkpoint="True"
+export load_depth_checkpoint="True"
 export depth_only="False"
 export no_classes=19
 
-export checkpoint_path="/mnt/Dataset/Logs/SSL/CPS/Semi/Semi-Supervision_Ratio16_22-Apr_21-23-nodebs2-tep35-lr0.002-maxdepth80_newcrfs/epoch-best_loss.pth"
-export depth_checkpoint_path="/mnt/Dataset/Logs/SSL/CPS/Semi/Concat-Semi-Supervision-FullResnet_Ratio16_DepthOnly_Pretrained-True_27-Apr_14-17-nodebs2-tep40-lr0.002-maxdepth80_newcrfs/epoch-best_loss.pth"
+export checkpoint_path="/mnt/Dataset/Logs/SSL/CPS/Semi/All_Semi-Supervision_Ratio16_10-May_19-19-nodebs2-tep100-lr0.02-maxdepth80_newcrfs/epoch-best_loss.pth"
+export depth_checkpoint_path="/mnt/Dataset/Logs/SSL/CPS/Semi/All-Labels-Concat-Semi-Supervision-FullResnet_Ratio16__DepthOnly_10-May_19-05-nodebs2-tep100-lr0.02-maxdepth80_newcrfs/epoch-best_loss.pth"
 
 export volna="/mnt/Dataset/city/"
 export OUTPUT_PATH='/mnt/Dataset/Logs/SSL/CPS/Semi/'
