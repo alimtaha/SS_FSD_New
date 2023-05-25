bec66d3b0b2f1509b82fda8c75096762dfa33a12
root <root@ip-10-0-0-45.eu-west-2.compute.internal>
2023-05-25 15:13:02 +0000

    small changes to configs

--- CPS/TorchSemiSeg/exp_city/city8_res50v3_CPS_CutMix/concat/script_depth_concat_fullresnet.sh
+++ CPS/TorchSemiSeg/exp_city/city8_res50v3_CPS_CutMix/concat/script_depth_concat_fullresnet.sh
@@ -2,11 +2,11 @@
 nvidia-smi
 
 export NGPUS=1
-export learning_rate=0.004
+export learning_rate=0.002
 export batch_size=2
 export snapshot_iter=2
-export epochs=100
-export ratio=16
+export epochs=107
+export ratio=8
 export CPU_DIST_ONLY='False'
 export WORLD_SIZE=1
 export mode="Concat-Semi-Supervision-FullResnet"
@@ -17,8 +17,8 @@ export load_depth_checkpoint="False"
 export depth_only="False"
 export no_classes=2
 
-export checkpoint_path="/mnt/Dataset/Logs/SSL/CPS/Semi/Semi-Supervision_Ratio16_22-Apr_21-23-nodebs2-tep35-lr0.002-maxdepth80_newcrfs/epoch-best_loss.pth"
-export depth_checkpoint_path="/mnt/Dataset/Logs/SSL/CPS/Semi/Concat-Semi-Supervision-FullResnet_Ratio16_DepthOnly_Pretrained-True_27-Apr_14-17-nodebs2-tep40-lr0.002-maxdepth80_newcrfs/epoch-best_loss.pth"
+export checkpoint_path="/mnt/Dataset/Logs/SSL/CPS/Semi/Semi-Supervision_Ratio8_13-May_13-25-nodebs2-tep40-lr0.002-maxdepth80_newcrfs/epoch-best_loss.pth"
+export depth_checkpoint_path="/mnt/Dataset/Logs/SSL/CPS/Semi/Concat-Semi-Supervision-FullResnet_Ratio8__DepthOnly_22-May_03-04-nodebs2-tep35-lr0.002-maxdepth80_newcrfs/epoch-best_loss.pth"
 
 export volna="/mnt/Dataset/city/"
 export OUTPUT_PATH='/mnt/Dataset/Logs/SSL/CPS/Semi/'
